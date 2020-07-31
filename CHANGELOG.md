04208ec6c8d93cf27dd90b089e6ac5c784e3db96
Mikołaj Wargowski <mikoajw20@gmail.com>
2020-07-31 08:58:36 +0200

    🎉 Project init

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 0.0.0 (2020-07-31)
+
+
+### Miscellaneous
+
+- Project init ([27d9ead](https://github.com/Exlabs/value-driven-templates/commit/27d9ead8bce182725e3bec657ee4da1651cffbe5))
+
---
+++ src/pages/_app.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import NextApp from 'next/app';
+import Head from 'next/head';
+
+import '../../.css/antd.less';
+
+export default class App extends NextApp {
+  componentDidMount() {
+    const jssStyles = document.querySelector('#jss-server-side');
+
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps, router } = this.props;
+
+    return (
+      <>
+        <Head>
+          <title>VDT | Value Driven Templates</title>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
+        </Head>
+        <Component {...pageProps} key={router.route} />
+      </>
+    );
+  }
+}
