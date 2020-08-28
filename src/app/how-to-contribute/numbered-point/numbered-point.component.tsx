a65c18ac9981051f4c6b4adfcd6f413fe10fa581
Mikołaj Wargowski <mikoajw20@gmail.com>
2020-08-28 08:27:17 +0200

    🎨 Styles improvements

--- src/app/how-to-contribute/numbered-point/numbered-point.component.tsx
+++ src/app/how-to-contribute/numbered-point/numbered-point.component.tsx
@@ -1,21 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Point = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 50px;
-  border-radius: 50%;
-  background-color: ${({ theme }) => theme.color.secondary};
-  width: 50px;
-  height: 50px;
-  color: ${({ theme }) => theme.color.white};
-  font-size: 31px;
-  font-weight: 700;
-
-  animation: pulse 3s linear infinite;
-`;
+import { Point } from './numbered-point.styles';
 
 interface NumberedPointProps {
   value: number;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,19 @@ import templates from '../content/templates.json';
 import { Home } from '../app/home/home.component';
 import { TemplateThumnbail } from '../app/home/thumbnail-list/thumbnail-list.component';
 import { AppContainer } from '../providers/layout.provider';
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+  position: relative;
+`;
+
+const BackgroundImage = styled.img`
+  position: absolute;
+  left: -10%;
+  top: -10%;
+  opacity: 0.05;
+  height: 60%;
+`;
 
 const HomePage = () => {
   const thumbnails = templates.map<TemplateThumnbail>((template) => ({
@@ -11,9 +24,15 @@ const HomePage = () => {
   }));
 
   return (
-    <AppContainer>
-      <Home thumbnails={thumbnails} />
-    </AppContainer>
+    <HomeContainer>
+      <BackgroundImage
+        alt="background"
+        src={`${process.env.ASSET_PREFIX}/icons/bg.svg`}
+      />
+      <AppContainer>
+        <Home thumbnails={thumbnails} />
+      </AppContainer>
+    </HomeContainer>
   );
 };
 
